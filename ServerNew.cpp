03b2a998d8ac05803df753d4c0a84e421d4e4276
Yevhen Shcherbak <35167188+YevhenShcherbak@users.noreply.github.com>
2021-09-15 20:48:27 +0300

    Start working with shared memory and threads

--- ServerNew.cpp
+++ ServerNew.cpp
@@ -1,4 +1,7 @@
 #include "ISocket.hpp"
+#include "Shm.hpp"
+#include <mutex>
+#include <thread>
 
 class Server : ISocket
 {
@@ -12,7 +15,7 @@ public:
         sendSocket = socket(PF_INET, SOCK_STREAM, 0);
 
         serverAddr.sin_family = AF_INET;
-        serverAddr.sin_port = htons(7500);
+        serverAddr.sin_port = htons(7523);
         serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
         memset(serverAddr.sin_zero, '\0', sizeof(serverAddr.sin_zero));
@@ -56,6 +59,7 @@ int main()
     int serverSocket;
     struct sockaddr_in serverAddr;
     socklen_t addrSize;
+    Shm shm(MEMORY_NAME);
 
     serverSocket = socket(PF_INET, SOCK_STREAM, 0);
 
@@ -65,7 +69,7 @@ int main()
 
     if (bind(serverSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0)
     {
-        std::cout << "Error with binding " << errno << std::endl;
+        std::cout << "Error with binding, Error#" << errno << std::endl;
     }
     if (listen(serverSocket, 15) == 0)
     {
@@ -80,7 +84,35 @@ int main()
     {
         memset(serverAddr.sin_zero, '\0', sizeof(serverAddr.sin_zero));
         addrSize = sizeof(serverAddr);
-        server.receiveSocket = accept(serverSocket, 0, 0);
-        std::cout << "Client> " << server.receiveMessage() << std::endl;
+        std::cout << "Thread1 working..." << std::endl;
+        std::thread th1([&server, &shm, serverSocket]()
+                        {
+                            std::mutex mtx;
+                            char message[BUF_SIZE];
+                            server.receiveSocket = accept(serverSocket, 0, 0);
+                            mtx.lock();
+                            memcpy(message, server.receiveMessage().c_str(), sizeof(server.receiveMessage()));
+                            shm.shm_write(message);
+                            std::cout << "Value from shm> " << shm.shm_read() << std::endl;
+                            mtx.unlock();
+                        });
+        th1.join();
+     //TODO: Make correct shared memory interactions
+        /*
+        mtx.lock();
+        std::thread th1([&serverSocket, &server]()
+                        {
+                            std::cout << "Enter in critical section..." << std::endl;
+                            std::cout << "Thread1 working..." << std::endl;
+                            server.receiveSocket = accept(serverSocket, 0, 0);
+                            std::cout << "Client> " << server.receiveMessage() << std::endl;
+                            sleep(10);
+                            std::cout << "Exit from critical section" << std::endl;
+                        });
+        mtx.unlock();
+        th1.join();
+        */
+        //server.receiveSocket = accept(serverSocket, 0, 0);
+        //std::cout << "Client> " << server.receiveMessage() << std::endl;
     }
 }
